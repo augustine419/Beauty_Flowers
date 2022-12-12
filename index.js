4ccb2ab66c7d6ac3e9a802b733a320a05a027c06
unknown <augustineameh1234@gmail.com>
2022-12-12 03:05:24 -0800

    second

---
+++ index.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector(".fa-bars")
+let trick = document.querySelector(".trick")
+
+menuIcon.addEventListener("click", function(){
+    // alert("hello ogoros");
+    trick.classList.toggle("overlay")
+})
+
